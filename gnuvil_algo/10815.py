c78c223dff8066f6efd1599dc8a106b87f755b15
kim-seonwoo <sw204011@gmail.com>
2024-11-21 12:03:18 +0900

    241121

---
+++ gnuvil_algo/10815.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+input = sys.stdin.readline  
+
+n = int(input())
+numbers = list(map(int, input().split()))
+m = int(input())
+arr = list(map(int, input().split()))
+result = []
+
+numbers.sort()
+
+
+
+for target in arr:
+    lo = -1
+    hi = n - 1
+    while lo + 1 < hi:
+        mid = (lo + hi) // 2
+        if numbers[mid] < target:  
+            lo = mid 
+        else:  
+            hi = mid
+    
+    if numbers[hi] == target:
+        result.append(1)
+    else:
+        result.append(0)
+    
+
+print(" ".join(map(str, result)))
